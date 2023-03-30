696e2808b578b899b7ab9ac6f7a1a1c69747c1e5
Vladimir RM <shvdrummer@gmail.com>
2023-03-30 13:58:47 +0300

    Create limit todos.

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 async function getAllTodos() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  const res = await fetch(
+    "https://jsonplaceholder.typicode.com/todos/?_limit=10"
+  );
   const todos = await res.json();
   console.log(todos);
 }
-
 
 window.addEventListener("DOMContentLoaded", getAllTodos);
