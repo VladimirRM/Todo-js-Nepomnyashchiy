c5787e0d93bf05a8e32a45f8fe2be1b9fb9ec10b
Vladimir RM <shvdrummer@gmail.com>
2023-03-30 15:01:23 +0300

    check responce .

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+document.getElementById("addTodo").addEventListener("click", async () => {
+  const input = document.getElementById("todoText");
+  const title = input.value;
+  if (title) {
+    const res = await fetch("https://jsonplaceholder.typicode.com/todos", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({ title, completed: false }),
+    });
+    const todo = await res.json();
+    console.log(todo);
+  }
+});
+console.log("addTodo");
+
 async function getAllTodos() {
   const res = await fetch(
     "https://jsonplaceholder.typicode.com/todos/?_limit=10"
@@ -16,7 +33,9 @@ function todoToHtml({ id, completed, title }) {
     `
   <div class="form-check" id='todo${id}'>
   <label class="form-check-label">
-    <input type="checkbox" class="form-check-input"  checked='${completed} '/>
+    <input type="checkbox" class="form-check-input"  ${
+      completed && "checked"
+    } />
   ${title}
   </label>
   <button
